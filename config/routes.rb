0af523bf6ba2ad35ef5f0ee18d2f672f270cae20
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-06 15:12:46 -0600

    Added routes
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+  get '/registration', to: 'users#new'
+  post '/registration', to: 'users#create'
+  get '/dashboard', to: 'users#show'
+
+
+  # get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
