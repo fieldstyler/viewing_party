4d164c250db84ffc87164b6c595d6e6f471c0a07
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-06 15:12:46 -0600

    created sessions controller/function
    Co-authored-by: <Name><Email>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  # def new
+  #   if session[:user_id] != nil
+  #     flash[:success] = "You are already logged in."
+  #     user = User.find(session[:user_id])
+  #   end
+  # end
+
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :new
+    end
+  end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   session[:cart] = nil
+  #   flash[:success] = "You've successfully logged out."
+  #   redirect_to '/'
+  # end
+end
