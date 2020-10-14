d3b588febff87858491cb82967c0ac9a0e38cbeb
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-14 17:26:27 -0600

    Add model test for user
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,8 @@ RSpec.describe User, type: :model do
     it {should have_many(:party_viewers)}
     it {should have_many(:parties).through(:party_viewers)}
   end
+
+  it "can be created" do
+    user = User.create(email: "sam@email.com", password: 'pass', password_confirmation: 'pass')
+  end
 end
