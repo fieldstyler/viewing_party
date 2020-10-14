a999d545b3a5d66010c19531018954f3cf40bb6b
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-14 15:38:16 -0600

    Develop create method that will create a new party and new partyviewer joins table object
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,8 +1,27 @@
 class PartyController < ApplicationController
 
   def new
-    # @movie = MovieFacade thing
-    # require "pry"; binding.pry
+    @movie = MovieService.movie_details(params[:id])
   end
 
+  def create
+    party = Party.new(party_params)
+    if party.save
+      PartyViewer.create(party_id: party.id, user_id: current_user.id, status: "host")
+      params[:friend_id].each do |friend_id|
+        PartyViewer.create(party_id: party.id, user_id: friend_id.to_i, status: 'invited')
+      end
+      flash[:success] = 'Viewing Party Created!'
+      redirect_to '/dashboard'
+    else
+      flash.now[:alert] = party.errors.full_messages.first
+      render :new
+    end
+  end
+
+  private
+
+  def party_params
+    params.permit(:movie, :duration, :party_date, :start_time)
+  end
 end
