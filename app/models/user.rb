110c00a15ea640e54c8a08cbb19ee4aa8f11b9f5
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-10-14 17:09:22 -0600

    Create methods to find viewing parties and movies
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
   has_many :party_viewers, dependent: :destroy
   has_many :parties, through: :party_viewers
+
+  def find_viewing_parties
+    PartyViewer.where(user_id: self.id)
+  end
+
+  def find_movies
+    movies = []
+    all_viewing_parties = find_viewing_parties
+    all_viewing_parties.each do |viewing_party|
+      movies << Party.find(viewing_party[:party_id])
+    end
+    return movies
+  end
 end
